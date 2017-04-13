a15e7542879ed495c48fba7708038948c5a7bbc2
Dunchadhn Lyons <dunlyons@stanford.edu>
2017-04-13 11:02:49 -0700

    Compulsive done

--- ggp-base-master/src/main/java/the_men_who_stare_at_goats.java
+++ ggp-base-master/src/main/java/the_men_who_stare_at_goats.java
@@ -1,7 +1,7 @@
 
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.ggp.base.apps.player.Player;
 import org.ggp.base.player.gamer.exception.GamePreviewException;
@@ -33,16 +33,59 @@ public class the_men_who_stare_at_goats extends StateMachineGamer {
 
 	}
 
+	private int maxscore(Role role, MachineState state, StateMachine machine)
+			throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
+		if(machine.isTerminal(state)) {
+			return machine.getGoal(state, role);
+		}
+
+		List<Move> moves = machine.getLegalMoves(state, role);
+		int score = 0;
+		for(int i = 0; i < moves.size(); i++) {
+			List<Move> list = new ArrayList<Move>();
+			list.add(moves.get(i));
+			int result = maxscore(role, machine.getNextState(state, list), machine);
+
+			if(result == 100) {
+				return 100;
+			} else if(result > score) {
+				score = result;
+			}
+		}
+		return score;
+	}
+
+	private Move bestmove(Role role, StateMachine machine)
+			throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
+		MachineState state = getCurrentState();
+		List<Move> moves = machine.getLegalMoves(state, role);
+		Move action = moves.get(0);
+		int score = 0;
+		for(int i = 0; i < moves.size(); i++) {
+			List<Move> list = new ArrayList<Move>();
+			list.add(moves.get(i));
+			int result = maxscore(role, machine.getNextState(state, list), machine);
+
+			if(result == 100) {
+				return moves.get(i);
+			} else if(result > score) {
+				score = result;
+				action = moves.get(i);
+			}
+		}
+
+
+		return action;
+	}
+
 	@Override
 	public Move stateMachineSelectMove(long timeout)
 			throws TransitionDefinitionException, MoveDefinitionException,
 			GoalDefinitionException {
 		StateMachine machine = getStateMachine();
-		MachineState state = getCurrentState();
 		Role role = getRole();
-		List<Move> moves = machine.getLegalMoves(state, role);
-		Random rn = new Random();
-		return moves.get(rn.nextInt(moves.size()));
+
+		return bestmove(role, machine);
 
 	}
 
