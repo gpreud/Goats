a921ee9a4806bce2056c439af746b856fc7fc642
Dunchadhn Lyons <dunlyons@stanford.edu>
2017-04-18 11:17:08 -0700

    Reverting

--- ggp-base-master/src/main/java/org/ggp/base/util/statemachine/implementation/prover/query/ProverQueryBuilder.java
+++ ggp-base-master/src/main/java/org/ggp/base/util/statemachine/implementation/prover/query/ProverQueryBuilder.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.ggp.base.util.gdl.grammar.GdlConstant;
 import org.ggp.base.util.gdl.grammar.GdlPool;
 import org.ggp.base.util.gdl.grammar.GdlProposition;
 import org.ggp.base.util.gdl.grammar.GdlRelation;
@@ -16,66 +15,64 @@ import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.Role;
 
 
-public final class ProverQueryBuilder
-{
+public final class ProverQueryBuilder {
+    private static final GdlRelation INIT_QUERY = GdlPool.getRelation(GdlPool.INIT, new GdlTerm[] { GdlPool.getVariable("?x") });
+    private static final GdlRelation NEXT_QUERY = GdlPool.getRelation(GdlPool.NEXT, new GdlTerm[] { GdlPool.getVariable("?x") });
+    private static final GdlRelation ROLE_QUERY = GdlPool.getRelation(GdlPool.ROLE, new GdlTerm[] { GdlPool.getVariable("?x") });
+    private static final GdlProposition TERMINAL_QUERY = GdlPool.getProposition(GdlPool.TERMINAL);
+    private static final GdlVariable VARIABLE = GdlPool.getVariable("?x");
 
-	private final static GdlConstant DOES = GdlPool.getConstant("does");
-	private final static GdlConstant GOAL = GdlPool.getConstant("goal");
-	private final static GdlRelation INIT_QUERY = GdlPool.getRelation(GdlPool.getConstant("init"), new GdlTerm[] { GdlPool.getVariable("?x") });
-	private final static GdlConstant LEGAL = GdlPool.getConstant("legal");
-	private final static GdlRelation NEXT_QUERY = GdlPool.getRelation(GdlPool.getConstant("next"), new GdlTerm[] { GdlPool.getVariable("?x") });
-	private final static GdlRelation ROLE_QUERY = GdlPool.getRelation(GdlPool.getConstant("role"), new GdlTerm[] { GdlPool.getVariable("?x") });
-	private final static GdlProposition TERMINAL_QUERY = GdlPool.getProposition(GdlPool.getConstant("terminal"));
-	private final static GdlVariable VARIABLE = GdlPool.getVariable("?x");
+    private ProverQueryBuilder() {
+    }
 
-	public static Set<GdlSentence> getContext(MachineState state)
-	{
-		return state.getContents();
-	}
+    public static Set<GdlSentence> getContext(MachineState state)
+    {
+        return state.getContents();
+    }
 
-	public static Set<GdlSentence> getContext(MachineState state, List<Role> roles, List<Move> moves)
-	{
-		Set<GdlSentence> context = new HashSet<GdlSentence>(state.getContents());
-		for (int i = 0; i < roles.size(); i++)
-		{
-			context.add(toDoes(roles.get(i), moves.get(i)));
-		}
-		return context;
-	}
+    public static Set<GdlSentence> getContext(MachineState state, List<Role> roles, List<Move> moves)
+    {
+        Set<GdlSentence> context = new HashSet<GdlSentence>(state.getContents());
+        for (int i = 0; i < roles.size(); i++)
+        {
+            context.add(toDoes(roles.get(i), moves.get(i)));
+        }
+        return context;
+    }
 
-	public static GdlRelation getGoalQuery(Role role)
-	{
-		return GdlPool.getRelation(GOAL, new GdlTerm[] { role.getName(), VARIABLE });
-	}
+    public static GdlRelation getGoalQuery(Role role)
+    {
+        return GdlPool.getRelation(GdlPool.GOAL, new GdlTerm[] { role.getName(), VARIABLE });
+    }
 
-	public static GdlRelation getInitQuery()
-	{
-		return INIT_QUERY;
-	}
+    public static GdlRelation getInitQuery()
+    {
+        return INIT_QUERY;
+    }
 
-	public static GdlRelation getLegalQuery(Role role)
-	{
-		return GdlPool.getRelation(LEGAL, new GdlTerm[] { role.getName(), VARIABLE });
-	}
+    public static GdlRelation getLegalQuery(Role role)
+    {
+        return GdlPool.getRelation(GdlPool.LEGAL, new GdlTerm[] { role.getName(), VARIABLE });
+    }
 
-	public static GdlRelation getNextQuery()
-	{
-		return NEXT_QUERY;
-	}
+    public static GdlRelation getNextQuery()
+    {
+        return NEXT_QUERY;
+    }
 
-	public static GdlRelation getRoleQuery()
-	{
-		return ROLE_QUERY;
-	}
+    public static GdlRelation getRoleQuery()
+    {
+        return ROLE_QUERY;
+    }
 
-	public static GdlProposition getTerminalQuery()
-	{
-		return TERMINAL_QUERY;
-	}
+    public static GdlProposition getTerminalQuery()
+    {
+        return TERMINAL_QUERY;
+    }
 
-	public static GdlRelation toDoes(Role role, Move move)
-	{
-		return GdlPool.getRelation(DOES, new GdlTerm[] { role.getName(), move.getContents() });
-	}
+    public static GdlRelation toDoes(Role role, Move move)
+    {
+        return GdlPool.getRelation(GdlPool.DOES, new GdlTerm[] { role.getName(), move.getContents() });
+    }
 
 }
